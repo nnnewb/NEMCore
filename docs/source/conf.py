52da16b62280b8941ad93f0dafd028c8d9d32d5c
weak_ptr <weak_ptr@163.com>
2019-11-23 12:55:39 +0800

    docs: update sphinx configuration

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -59,9 +59,13 @@ exclude_patterns = []
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'insegel'
+html_theme = 'groundwork'
 
-html_theme_options = {'wending_last_updated': False}
+html_theme_options = {
+    "sidebar_width": '240px',
+    "stickysidebar": True,
+    "stickysidebarscrollable": True,
+}
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
